e11b99690284ce78b7810c96d1ee0d2edd9982e0
Luis Felipe Morales <145800266+LMORALESDEV@users.noreply.github.com>
2025-06-13 17:49:40 -0400

    Ejercicio pratica 2 paso a paso

---
+++ Actividad3LengPro3/Actividad3LengPro3/Controllers/EstudiantesController.cs
@@ -0,0 +1,82 @@
+﻿using Actividad3LengPro3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Actividad3LengPro3.Controllers
+{
+    public class EstudiantesController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+        
+            private static List<EstudiantesViewModel> estudiantes = new List<EstudiantesViewModel>();
+
+        // GET: Estudiantes
+        public ActionResult Registrar()
+        {
+            return View(new EstudiantesViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Registrar(EstudiantesViewModel estudiante)
+        {
+            if (estudiante.EstaBecado == false)
+            {
+                estudiante.PorcentajeBeca = null;
+            }
+
+            if (ModelState.IsValid)
+            {
+                estudiantes.Add(estudiante);
+                return RedirectToAction("Lista");
+            }
+
+            return View("Index", estudiante);
+        }
+
+        public ActionResult Lista()
+        {
+            return View(estudiantes);
+        }
+
+        public ActionResult Editar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante == null)
+                return View();
+
+            return View(estudiante);
+        }
+
+        [HttpPost]
+        public ActionResult Editar(EstudiantesViewModel estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                var existente = estudiantes.FirstOrDefault(e => e.Matricula == estudiante.Matricula);
+                if (existente != null)
+                {
+                    estudiantes.Remove(existente);
+                    estudiantes.Add(estudiante);
+                }
+                return RedirectToAction("Lista");
+            }
+
+            return View(estudiante);
+        }
+
+        public ActionResult Eliminar(string matricula)
+        {
+            var estudiante = estudiantes.FirstOrDefault(e => e.Matricula == matricula);
+            if (estudiante != null)
+            {
+                estudiantes.Remove(estudiante);
+            }
+            return RedirectToAction("Lista");
+        }
+    }
+
+
+    }
+
---
+++ Actividad3LengPro3/Actividad3LengPro3/Models/EstudiantesViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Actividad3LengPro3.Models
+{
+    public class EstudiantesViewModel
+    {
+        [Required(ErrorMessage ="El nombre del estudiante es obligatorio.")]
+        [StringLength(100)]
+        public string NombreCompleto { get; set; }
+
+        [Required(ErrorMessage = "La matricula del estudiante es obligatoria.")]
+        [StringLength(15, MinimumLength = 6)]
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "La carrera del estudiante es obligatoria.")]
+        public string Carrera { get; set; }
+
+        [Required(ErrorMessage = "El correo del estudiante es obligatorio.")]
+        [EmailAddress]
+        public string CorreoInstitucional { get; set; }
+
+        [Phone(ErrorMessage = "El telefono del estudiante es obligatorio.")]
+        [MinLength(10)]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "la fecha del estudiante es obligatoria.")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        [Required(ErrorMessage = "El genero del estudiante es obligatorio.")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "El turno del estudiante es obligatorio.")]
+        public string Turno { get; set; }
+
+        [Required(ErrorMessage = "El tipo de ingreso del estudiante es obligatorio.")]
+        public string TipoIngreso { get; set; }
+
+        public bool EstaBecado { get; set; }
+
+        [Range(0, 100, ErrorMessage = "Debe estar entre 0 y 100")]
+        public int? PorcentajeBeca { get; set; }
+
+        [Range(typeof(bool), "true", "true", ErrorMessage = "Debe aceptar los términos.")]
+        public bool TerminosYCondiciones { get; set; }
+    }
+}
